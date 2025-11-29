3fe2b90725140f5ef6cf49097ba1285e718cb893
Gabriel Melo <gblsmlo@gmail.com>
2025-11-29 01:24:45 -0300

    feat: add `auth.jpg` background image to authentication layout and adjust grid structure

--- src/modules/auth/components/auth-layout.tsx
+++ src/modules/auth/components/auth-layout.tsx
@@ -1,4 +1,5 @@
 import { Logo } from '@components/logo'
+import Image from 'next/image'
 import Link from 'next/link'
 import type { ReactNode } from 'react'
 import AuthGuard from './auth-guard'
@@ -6,8 +7,8 @@ import AuthGuard from './auth-guard'
 export function AuthLayout({ children }: { children: ReactNode }) {
 	return (
 		<AuthGuard>
-			<div className="grid min-h-svh lg:grid-cols-3">
-				<div className="flex flex-col gap-4 p-6 md:p-10 lg:col-span-2">
+			<div className="grid min-h-svh lg:grid-cols-5">
+				<div className="flex flex-col gap-4 p-6 md:p-10 lg:col-span-3">
 					<div className="flex justify-center gap-2 md:justify-start">
 						<Link href="/">
 							<Logo />
@@ -17,8 +18,8 @@ export function AuthLayout({ children }: { children: ReactNode }) {
 						<div className="w-full max-w-sm">{children}</div>
 					</div>
 				</div>
-				<div className="relative hidden rounded-2xl bg-muted lg:m-6 lg:block">
-					<div className="relative hidden bg-muted lg:block" />
+				<div className="relative hidden overflow-hidden rounded-2xl bg-foreground lg:col-span-2 lg:m-6 lg:block">
+					<Image alt="Login" className="object-cover opacity-70" fill src="/auth.jpg" />
 				</div>
 			</div>
 		</AuthGuard>
